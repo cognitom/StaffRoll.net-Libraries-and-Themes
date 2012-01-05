db2f65165c90dc90a03716d70a306a846f39481d
Tsutomu Kawamura <kawamura@cognitom.com>
2012-01-05 10:34:36 +0900

    First commit

---
+++ httpdocs/github/theme/underground/1.0/main.js
@@ -0,0 +1,68 @@
+StaffRoll.MyTheme = function(){
+	var parent = new StaffRoll.Theme();
+	parent.constructor.apply(this);
+	/* Fields */
+	this.id = 'underground';
+	this.type = 'fixed';
+	this.photo = false;
+	this.preloaded = false;
+	this.divOuter = null;
+	this.divInner = null;
+	/* Constructor */
+	this.initialize = function(){
+		parent.initialize.call();
+		var d0 = document.createElement('div');
+			d0.setAttribute('id', 'srBox');
+			d0.style.display = 'none';
+		var d1 = document.createElement('div');
+			d1.setAttribute('id', 'srBoxInner');
+		var d2 = document.createElement('div');
+			d2.setAttribute('id', 'srBoxClose');
+			d2.innerHTML = 'CLOSE';
+			d2.onclick = function(){ StaffRoll.credit.hide(); };
+		d0.appendChild(d1);
+		d0.appendChild(d2);
+		document.getElementsByTagName('body')[0].appendChild(d0);
+		this.divOuter = d0;
+		this.divInner = d1;
+		return this;
+	};
+	/* Methods */
+	this.show = function(){
+		if (!this.preloaded) this.preload();
+		this.divOuter.style.display="block";
+		var mytheme = this;
+		(new StaffRoll.Effect()).initialize(
+			0, this.divInner.clientHeight,
+			(function(){ return function(p){ mytheme.divOuter.style.height = p+'px'; }})()
+		);
+		var currentPos = document.body.parentNode.scrollTop || document.body.scrollTop || document.documentElement.scrollTop;
+		(new StaffRoll.Effect()).initialize(
+			currentPos, currentPos+this.divInner.clientHeight,
+			(function(){ return function(p){ scrollTo(0, p); }})()
+		);
+	};
+	this.hide = function(){
+		var mytheme = this;
+		(new StaffRoll.Effect()).initialize(
+			this.divInner.clientHeight, 0,
+			(function(){ return function(p){ mytheme.divOuter.style.height = p+'px'; }})(),
+			(function(){ return function(){ StaffRoll.theme.divOuter.style.display="none"; }})()
+		);
+	};
+	this.preload = function(){
+		var html = '';
+		html += '<h2><strong>'+StaffRoll.credit.repo+'</strong> / contributors</h2>';
+		html += '<div>';
+		html += '<ul>';
+		for (var j=0; j<StaffRoll.credit.creators.length; j++){
+			var creator = StaffRoll.credit.creators[j];
+			html += '<li style="background-image:url('+creator.avatar_url+')"><a href="https://github.com/'+creator.login+'" target="'+creator.login+'">'+creator.login+'</a></li>';
+		}
+		html += '</ul>';
+		html += '</div>';
+		this.divInner.innerHTML = html;
+		this.preloaded = true;
+	};
+};
+StaffRoll.credit.loadTheme((new StaffRoll.MyTheme()).initialize());
---
+++ httpdocs/website/script/1.0/load.js
@@ -0,0 +1,244 @@
+var StaffRoll = {};//namespace
+
+/**
+ * Settings
+ */
+//StaffRoll.net = 'staffroll.net';
+StaffRoll.net = 'staffroll.site';
+
+/**
+ * Classes
+ */
+StaffRoll.Credit = function(){
+	this.anchors = [];
+	/* Fields */
+	this.id = '';
+	this.base_url = '';
+	this.twitter = '';
+	this.movie_url = '';
+	this.authorized = false;
+	this.authorized_date = '';
+	this.pages = [];
+	this.loading = false;
+	this.jsonp_ready = false;
+	this.theme_ready = true;
+	this.opened = false;
+	/* Constructor */
+	this.initialize = function(){
+		return this;
+	};
+	this.addAnchor = function(anchor){
+		if(anchor.className == 'staffroll'){
+			var id = (tmp = anchor.href.match(/^https?:\/\/(beta\.)?staffroll\.(net|site)\/site\/([a-z0-9\-]*)\//)) ? tmp[3] : '';
+			if (this.id == '') this.id = id;
+			else if (this.id != id) return;
+		} else {
+			this.url = encodeURIComponent(anchor.href);
+		}
+		
+		this.anchors.push(anchor);
+		if (!this.id && !this.url) return this;
+		anchor.onclick = function(e){ StaffRoll.credit.showOrHide(); return false; };
+		return this;
+	};
+	/* Methods */
+	this.callTheme = function(){
+		var script, link;
+		var scripts = document.getElementsByTagName("script");
+		for (var i=0; i<scripts.length; i++){
+			if (scripts[i].src && scripts[i].src.match(/^https?:\/\/include\.staffroll\.(net|site)\/script\/1\.0\/load\.js(\?.*)?$/)){
+				script = scripts[i];
+			}
+		}
+		var theme = (tmp = script.src.match(/\?.*theme=([a-z0-9_]*)/)) ? tmp[1] : 'default';
+		if (theme != 'default'){
+			this.theme_ready = false;
+			script = document.createElement('script');
+				script.setAttribute('type', 'text/javascript');
+				script.setAttribute('src', 'http://include.'+StaffRoll.net.replace('beta.','')+'/theme/'+theme+'/1.0/main.js');
+			link = document.createElement('link');
+				link.setAttribute('rel', 'stylesheet');
+				link.setAttribute('type', 'text/css');
+				link.setAttribute('href', 'http://include.'+StaffRoll.net.replace('beta.','')+'/theme/'+theme+'/1.0/main.css');
+			document.getElementsByTagName('head')[0].appendChild(script);
+			document.getElementsByTagName('head')[0].appendChild(link);
+		}
+	};
+	this.callAPI = function(){
+		var script = document.createElement('script');
+			script.setAttribute('type', 'text/javascript');
+			script.setAttribute('src', 'http://api.'+StaffRoll.net.replace('beta.','')+'/1.0/?'+(this.url ? 'url='+ this.url : 'site='+this.id) );
+		document.getElementsByTagName('head')[0].appendChild(script);
+	};
+	this.loadTheme = function(theme){
+		StaffRoll.theme = theme;
+		this.theme_ready = true;
+		this.showIfReady();
+	};
+	this.loadJsonp = function(data){
+		this.id = data.id;
+		this.url = data.url;
+		this.twitter = data.twitter;
+		this.template = data.template;
+		this.movie_url = data.movie_url;
+		this.authorized = data.authorized;
+		this.authorized_date = data.authorized_date;
+		for (var i=0; i<data.pages.length; i++){
+			this.pages.push((new StaffRoll.Page()).initialize(data.pages[i]));
+		}
+		this.jsonp_ready = true;
+		this.showIfReady();
+	};
+	this.showIfReady = function(){
+		if (!this.opened && this.jsonp_ready && this.theme_ready){
+			this.loading = false;
+			this.show();
+		}
+	};
+	this.showOrHide = function(){
+		if (!this.opened) this.show();
+			else this.hide();
+	};
+	this.show = function(){
+		if (!this.jsonp_ready && !this.loading){
+			this.loading = true;
+			this.callTheme();
+			this.callAPI();
+			return;
+		}
+		this.opened = true;
+		StaffRoll.theme.show();
+	};
+	this.hide = function(){
+		this.opened = false;
+		StaffRoll.theme.hide();
+	};
+};
+
+StaffRoll.Page = function(){
+	/* Fields */
+	this.id = '';
+	this.name = '';
+	this.photo_url = '';
+	this.creators = [];
+	/* Constructor */
+	this.initialize = function(data){
+		this.id = data.id;
+		this.name = data.name;
+		this.photo_url = data.photo_url;
+		for (var i=0; i<data.creators.length; i++)
+			this.creators.push((new StaffRoll.Creator()).initialize(data.creators[i]));
+		return this;
+	};
+};
+
+StaffRoll.Creator = function(){
+	/* Fields */
+	this.id = '';
+	this.name = '';
+	this.role = '';
+	this.company = '';
+	this.comment = '';
+	this.user_id = '';
+	this.authorized = false;
+	/* Constructor */
+	this.initialize = function(data){
+		this.id = data.id;
+		this.name = data.name;
+		this.role = data.role;
+		this.company = data.company;
+		this.comment = data.comment;
+		this.user_id = data.user_id;
+		this.authorized = data.authorized;
+		return this;
+	};
+	/* Methods */
+	this.getUrl = function(){
+		return 'http://'+StaffRoll.net+'/user/'+this.user_id+'/';
+	};
+};
+
+StaffRoll.Theme = function(){
+	/* Fields */
+	this.id = 'default';
+	this.type = 'fixed';
+	this.photo = false;
+	/* Constructor */
+	this.initialize = function(){
+		return this;
+	};
+	/* Methods */
+	this.show = function(){
+		var str = '';
+		for (var i=0; i<StaffRoll.credit.pages.length; i++){
+			var page = StaffRoll.credit.pages[i];
+			for (var j=0; j<page.creators.length; j++){
+				var creator = page.creators[j];
+				str += '[' + creator.role + ']\n'
+					+ creator.name
+					+ (creator.company?' ('+creator.company+')':'')
+					+ '\n\n';
+			}
+		}
+		alert(str);
+	};
+	this.hide = function(){};
+};
+
+StaffRoll.Effect = function(){
+	/* Fileds */
+	this.interval = 25;
+	this.msMax = 1000;
+	this.msStart = 0;
+	this.start = 0;
+	this.end = 0;
+	this.timer = 0;
+	this.onUpdate = function(p){};
+	this.onEndEffect = function(){};
+	/* Constructor */
+	this.initialize = function(startVal, endVal, onUpdate, onEndEffect, duration){
+		var d = new Date();
+		this.msStart = d.getTime();
+		this.start = startVal;
+		this.end = endVal;
+		if (onUpdate) this.onUpdate = onUpdate;
+		if (onEndEffect) this.onEndEffect = onEndEffect;
+		if (duration) this.msMax = duration;
+		var target = this;
+		var ef = (function(){ return function(){ target.effect(); } })();
+		this.timer = setInterval(ef,this.interval);
+		return this
+	};
+	/* Methods */
+	this.effect = function(){
+		var d = new Date();
+		var ms = d.getTime() - this.msStart;
+		var MSm = this.msMax;
+		if (ms > MSm) ms = MSm;
+		var L = this.end - this.start;
+		var x = (1-Math.cos(Math.PI*ms/MSm))*0.5*L;
+		this.onUpdate(this.start+x);
+		if (ms == MSm) {
+			clearInterval(this.timer);
+			this.timer = 0;
+			this.onEndEffect();
+		}
+	};
+};
+
+/**
+ * Setups
+ */
+(function(){
+	var setup = function(){
+		StaffRoll.theme = (new StaffRoll.Theme()).initialize();
+		StaffRoll.credit = (new StaffRoll.Credit()).initialize();
+		var anchors = document.getElementsByTagName("a");
+		for (var i=0; i<anchors.length; i++)
+			if (anchors[i].className && anchors[i].className == 'staffroll')
+				StaffRoll.credit.addAnchor(anchors[i]);
+	}
+	if (window.attachEvent) window.attachEvent('onload', setup);//IE
+		else if (window.addEventListener) window.addEventListener('load', setup, true);//W3C
+			else window.onload = setup;
+})();
